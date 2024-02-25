8ba8b03017f623f1dc2c4b299d00d011fd00332d
este6an13 <diquintero@unal.edu.co>
2024-02-24 19:59:33 -0500

    checks-ocr\feat(build): parse arguments when running with docker
    - add arguments parser to `run.sh`

--- run.sh
+++ run.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# Set default values for arguments
+UPDATE=()
+MODEL_NAME=""
+LLM=""
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --update)
+            UPDATE+=("$2")
+            shift
+            ;;
+        --model-name)
+            MODEL_NAME="$2"
+            shift
+            ;;
+        --llm)
+            LLM="--llm"
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Run the Docker command
 sudo docker run \
     -v .:/checks \
     -v /checks-ocr/images:/checks-ocr/images \
@@ -7,4 +35,4 @@ sudo docker run \
     -v checks-ocr-llm-cache:/checks-ocr/src/llm/cache \
     -v checks-ocr-tecxtract-cache:/checks-ocr/cache \
     -v checks-ocr-data:/checks-ocr/data/data \
-    checks-ocr
+    checks-ocr $LLM "${UPDATE[@]}" --model-name="$MODEL_NAME"
